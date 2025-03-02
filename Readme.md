0357250112b884544ba2831d5faaef0a39f7b2cd
Srivatsa J <srivatsa1199@gmail.com>
2025-03-02 12:48:32 +0530

    Add readme and allow user input

---
+++ Readme.md
@@ -0,0 +1,10 @@
+## Rate Limiters
+
+1. Token Bucket
+2. Leaky Bucket
+3. Fixed Window Counter
+4. Sliding Window Log
+5. Sliding Window Counter
+
+#### How to run ?
+> On the terminal, run the `Rate-Limiters` binary and enter the possible options of the algorithms
--- main.go
+++ main.go
@@ -1,9 +1,50 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+
 	"github.com/srivatsa17/Rate-Limiters/rate_limiters"
 )
 
 func main() {
-	rate_limiters.TokenBucketRateLimiter()
+	fmt.Println("Welcome to Rate Limiting\nPossible Options are:")
+	fmt.Println("1. Token Bucket\n2. Leaky Bucket\n3. Fixed Window Counter\n4. Sliding Window Log\n5. Sliding Window Counter")
+	fmt.Println("Please enter the option number of the Rate limiter algorithm you want to try out:")
+
+	input, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	if err != nil {
+		fmt.Println("Error reading input:", err)
+		os.Exit(1)
+	}
+
+	input = input[:len(input)-1] // Remove newline character
+	choice, err := strconv.Atoi(input)
+	if err != nil {
+		fmt.Println("Invalid input. Please enter a valid number.")
+		os.Exit(1)
+	}
+
+	switch choice {
+	case 1:
+		fmt.Println("You selected Token Bucket Rate Limiter")
+		rate_limiters.TokenBucketRateLimiter()
+	case 2:
+		fmt.Println("You selected Leaky Bucket Rate Limiter")
+		rate_limiters.LeakyBucketRateLimiter()
+	case 3:
+		fmt.Println("You selected Fixed Window Counter Rate Limiter")
+		// rate_limiters.FixedWindowCounter()
+	case 4:
+		fmt.Println("You selected Sliding Window Log Rate Limiter")
+		// rate_limiters.SlidingWindowLog()
+	case 5:
+		fmt.Println("You selected Sliding Window Counter Rate Limiter")
+		// rate_limiters.SlidingWindowCounter()
+	default:
+		fmt.Println("Invalid option. Please select a number between 1 and 5.")
+		os.Exit(1)
+	}
 }
